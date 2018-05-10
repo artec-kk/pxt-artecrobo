600012d50ce299ac6213af5d4cd37c9164b1357e
Wakabayashi, Kenichi <k-wakabayashi@artec-kk.co.jp>
2018-05-10 18:31:26 +0900

    アイコン指定変更

--- artecrobo.ts
+++ artecrobo.ts
@@ -31,7 +31,7 @@ enum connectorServoMotor {
 /**
  * ArtecRobo control package
  */
-//% color=190 weight=100 icon="\uf1ec" block="ArtecRobo"
+//% color=190 weight=100 icon="icon.png" block="ArtecRobo"
 namespace artecrobo {
 
 	/* spped initial value */
