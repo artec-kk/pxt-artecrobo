7b42dde25f22f739ecd4276f8f91978fbdc62f0e
Wakabayashi, Kenichi <k-wakabayashi@artec-kk.co.jp>
2018-05-15 18:56:07 +0900

    色々修正してみた

--- artecrobo.ts
+++ artecrobo.ts
@@ -196,18 +196,18 @@ namespace artecrobo {
 		let divideP15 = maxData / diffP15;	// 1度変化させる間隔
 
 		for(i = 0; i <= maxData; i++ ) {
-			if( i % divideP13 == 0 ){
+			// if( i % divideP13 == 0 ){
 				angleP13 = angleP13 + dirP13;
 				pins.servoWritePin(AnalogPin.P13, angleP13);
-			}
-			if( i % divideP14 == 0 ){
-				angleP14 = angleP14 + dirP14;
-				pins.servoWritePin(AnalogPin.P14, angleP14);
-			}
-			if( i % divideP15 == 0 ){
-				angleP15 = angleP15 + dirP15;
-				pins.servoWritePin(AnalogPin.P15, angleP15);
-			}
+			// }
+			// if( i % divideP14 == 0 ){
+			// 	angleP14 = angleP14 + dirP14;
+			// 	pins.servoWritePin(AnalogPin.P14, angleP14);
+			// }
+			// if( i % divideP15 == 0 ){
+			// 	angleP15 = angleP15 + dirP15;
+			// 	pins.servoWritePin(AnalogPin.P15, angleP15);
+			// }
 			basic.pause(interval)
 		}
 		// 最後に全部そろえる。
