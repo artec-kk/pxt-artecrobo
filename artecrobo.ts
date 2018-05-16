d33988d5a8a45f7d27ecb5e7c6dfc22068f0accd
Wakabayashi, Kenichi <k-wakabayashi@artec-kk.co.jp>
2018-05-16 11:23:53 +0900

    言語リソース更新

--- artecrobo.ts
+++ artecrobo.ts
@@ -124,7 +124,7 @@ namespace artecrobo {
 	let angleP15 = 0;
     //% weight=84
 	//% blockId=artec_move_servo_motor
-	//% block="Set servomotor %connector| to %angle| degree with speed %_speed"
+	//% block="Set servomotor %connector| to %angle| degree by speed %_speed"
 	//% speed.min=0 speed.max= 1023
 	export function moveServoMotor(_connector: connectorServoMotor, _angle: number, _speed: number): void {
 		switch (_connector) {
@@ -150,7 +150,7 @@ namespace artecrobo {
 	 */
     //% weight=84
     //% blockId=artec_async_move_servo_motor
-    //% block="Move servomotor synchronously| speed as %_speed |set P13 to %_angle13|degree| Servo motor P14 set angle as %_angle14 |Servo motor P15 set angle as %_angle15"
+    //% block="Move servomotor synchronously| speed as %_speed| Angle of P13:%_angle13| Angle of P14:%_angle14 |Angle of P15:%_angle15"
     //% _speed.min=1 _speed.max=20
     //% _angle13.min=0 _angle13.max=180
     //% _angle14.min=0 _angle14.max=180
